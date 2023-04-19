ace5ab3469242d529129fb4871459ba6fa3fd165
nac-39 <nanaco.saitoh39@gmail.com>
2023-04-19 20:29:15 +0900

    feat: workcard 調整

--- components/FrameworkTag.vue
+++ components/FrameworkTag.vue
@@ -4,22 +4,18 @@
     :href="url"
     target="_brank"
     rel="noopener noreferrer"
-    class="rounded-full bg-main_darkest flex items-center h-7"
+    class="flex items-center h-7"
   >
-    <span
-      class="bg-bg_light rounded-full h-5 w-5 my-1 ml-1 flex items-center justify-center"
-    >
+    <span class="h-5 w-5 my-1 ml-1 flex items-center justify-center">
       <Icon :name="icon" class="h-4 w-4" />
     </span>
-    <span class="text-text_white text-sm p-1">{{ name }}</span>
+    <span class="text-text_main text-sm p-1">{{ name }}</span>
   </a>
-  <span v-else class="rounded-full bg-main_darkest flex items-cent h-7">
-    <span
-      class="bg-bg_light rounded-full h-5 w-5 m-1 flex items-center justify-center"
-    >
+  <span v-else class="flex items-cent h-7">
+    <span class="h-5 w-5 m-1 flex items-center justify-center">
       <Icon :name="icon" class="h-4 w-4" />
     </span>
-    <span class="text-text_white text-sm p-1">{{ name }}</span>
+    <span class="text-text_main text-sm p-1">{{ name }}</span>
   </span>
 </template>
 <script setup lang="ts">
--- components/WorkCard.vue
+++ components/WorkCard.vue
@@ -1,35 +1,33 @@
 <template>
-  <div>
-    <div class="border border-text_main sm:w-80">
-      <nuxt-img
-        v-if="imgPath"
-        :src="imgPath"
-        class="aspect-[16/9] border-b border-text_main"
-      />
-      <div class="mt-2">
-        <div class="flex items-center flex-wrap justify-between mx-2">
-          <span class="text-lg font-bold">
-            {{ title }}
-          </span>
-          <span class="text-sm min-w-max">
-            {{ date }}
-          </span>
-        </div>
-        <div class="text-sm prose m-2">
-          {{ description }}
-        </div>
-        <div class="flex m-2 flex-wrap">
-          <div
-            v-for="framework in frameworks"
-            :key="framework.name"
-            class="flex p-1"
-          >
-            <FrameworkTag
-              :name="framework.name"
-              :icon="framework.icon"
-              :url="framework.url"
-            />
-          </div>
+  <a :href="siteUrl" class="block border border-text_main sm:w-80">
+    <nuxt-img
+      v-if="imgPath"
+      :src="imgPath"
+      class="aspect-[16/9] border-b border-text_main"
+    />
+    <div class="mt-2">
+      <div class="flex items-center flex-wrap justify-between mx-2">
+        <span class="text-lg font-bold">
+          {{ title }}
+        </span>
+        <span class="text-sm min-w-max">
+          {{ date }}
+        </span>
+      </div>
+      <div class="text-sm prose m-2">
+        {{ description }}
+      </div>
+      <div class="flex m-2 flex-wrap">
+        <div
+          v-for="framework in frameworks"
+          :key="framework.name"
+          class="flex p-1"
+        >
+          <FrameworkTag
+            :name="framework.name"
+            :icon="framework.icon"
+            :url="framework.url"
+          />
         </div>
       </div>
     </div>
@@ -37,21 +35,12 @@
       <a
         v-if="githubUrl"
         :href="githubUrl"
-        :class="siteUrl ? ' basis-1/2' : 'w-full'"
-        class="border border-t-0 border-text_main text-center"
+        class="border-t w-full border-text_main text-center"
       >
         GitHub
       </a>
-      <a
-        v-if="siteUrl"
-        :href="siteUrl"
-        :class="githubUrl ? ' border-l-0 basis-1/2' : 'w-full'"
-        class="border border-t-0 border-text_main text-center"
-      >
-        Site
-      </a>
     </div>
-  </div>
+  </a>
 </template>
 <script setup lang="ts">
 import type { FrameworkTag } from "~/types/Skills"
