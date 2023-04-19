58634cd41f7fc9c6d0cb80efe4f1a7371a1dbb02
nac-39 <nanaco.saitoh39@gmail.com>
2023-04-19 23:11:12 +0900

    feat: add ogp

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,3 +7,18 @@
     <UiFooter />
   </div>
 </template>
+<script setup lang="ts">
+const router = useRouter()
+const currentPath = computed(
+  () => `https://nac-39.github.io/Portfolio/${router.currentRoute.value.path}`
+)
+useHead({
+  meta: [
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: currentPath.value
+    }
+  ]
+})
+</script>
