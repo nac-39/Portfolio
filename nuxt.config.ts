430f5f2e6f4bcaf3ae30e382ff064613e704aff4
nac-39 <nanaco.saitoh39@gmail.com>
2023-04-19 18:57:11 +0900

    fix: cdnURL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: "/Portfolio",
-    cdnURL: "/"
+    baseURL: "/Portfolio"
   },
   css: ["@/assets/css/tailwind.css"],
   modules: ["@nuxt/content", "nuxt-icon", "@nuxt/image-edge"],
