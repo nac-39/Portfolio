cdce286220e810666f55a1d13f22513d31015cdf
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-07-14 13:57:17 +0900

    feat: redirect, 404ページの実装

---
+++ pages/404.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center justify-center w-full h-full">
+    <h1 class="font-title font-bold text-3xl">
+      404 - Page Not Found
+    </h1>
+  </div>
+</template>
