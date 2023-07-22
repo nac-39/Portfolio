a75825e1c0b0aad7110046f5288a414315cc0cba
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-07-22 15:38:22 +0900

    feat: h1/2の下線を削除、マージン、行幅を調整

--- pages/blog/[year]/[month]/[day]/[slug].vue
+++ pages/blog/[year]/[month]/[day]/[slug].vue
@@ -1,17 +1,14 @@
 <template>
-  <main class="max-w-3xl md:m-auto p-4">
-    <div class="text-sm py-2">
+  <main class="max-w-2xl m-auto p-4">
+    <div class="text-sm my-2">
       {{ showDate }}
     </div>
     <div>
-      <h1 class="text-4xl pt-2 pb-8 font-semibold">
+      <h1 class="text-4xl mt-8 mb-16 font-semibold">
         {{ article.title }}
       </h1>
-      <article
-        class="prose leading-7 markdown-body"
-        style="color-scheme: light"
-      >
-        <ContentRenderer :value="article" class="space-y-2" />
+      <article class="leading-7 markdown-body" style="color-scheme: light">
+        <ContentRenderer :value="article" />
       </article>
     </div>
   </main>
