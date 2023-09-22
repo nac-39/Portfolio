83378b282a3cd27e4e218e4f9d2fa641b5313236
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-09-22 22:08:09 +0900

    feat: add image component with caption

---
+++ components/content/CaptionImage.vue
@@ -0,0 +1,35 @@
+<template>
+  <figure>
+    <slot />
+    <figcaption class="text-xs text-center text-text_gray">
+      <div>
+        <slot name="caption" />
+      </div>
+    </figcaption>
+  </figure>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  src: {
+    type: String,
+    default: "",
+  },
+  alt: {
+    type: String,
+    default: "",
+  },
+  width: {
+    type: [String, Number],
+    default: undefined,
+  },
+  height: {
+    type: [String, Number],
+    default: undefined,
+  },
+  caption: {
+    type: String,
+    default: "",
+  },
+})
+</script>
