670e9cd064d5251640e55ceeb23e60dc1bfe190c
nac-39 <83295760+nac-39@users.noreply.github.com>
2023-10-13 12:28:40 +0900

    feat: 各コンポーネントに縦棒を追加

--- components/Experience/ExperienceCard.vue
+++ components/Experience/ExperienceCard.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="flex items-center">
-    <div class="m-4">
+    <div class="m-4 w-32">
       {{ period }}
     </div>
     <div
-      class="bg-slate-50 rounded-full w-4 h-4 border-main_darkest border-4"
+      class="bg-slate-50 rounded-full w-4 h-4 border-main_darkest border-4 z-10"
     />
+    <div class="relative h-full">
+      <div class="h-full bg-main_darkest w-1 absolute right-1.5" />
+    </div>
     <div class="bg-main_darkest h-1 w-8" />
-    <div class="border-main_darkest border-4 w-64">
+    <div class="border-main_darkest border-4 w-64 mt-2 mb-2">
       <div class="text-lg font-bold m-2">
         {{ title }}
       </div>
--- components/Experience/index.vue
+++ components/Experience/index.vue
@@ -1,21 +1,13 @@
 <template>
-  <div class="relative h-full">
-    <div class="flex h-full justify-center">
-      <div class="flex flex-col items-center h-full absolute right-24 left-0">
-        <div
-          class="bg-slate-50 rounded-full w-4 h-4 border-main_darkest border-4"
-        />
-        <div class="bg-main_darkest w-1 h-full" />
-      </div>
-      <div class="flex flex-col space-y-4 items-end z-10">
-        <ExperienceCard
-          v-for="ex in experiences"
-          :key="ex.title"
-          :period="ex.period"
-          :title="ex.title"
-          :description="ex.description"
-        />
-      </div>
+  <div class="flex h-full justify-center">
+    <div class="flex flex-col items-end z-10">
+      <ExperienceCard
+        v-for="ex in experiences"
+        :key="ex.title"
+        :period="ex.period"
+        :title="ex.title"
+        :description="ex.description"
+      />
     </div>
   </div>
 </template>
